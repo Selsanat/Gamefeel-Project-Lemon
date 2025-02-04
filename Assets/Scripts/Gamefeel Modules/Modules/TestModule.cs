03d6f743632d2609195d11da049ee4029a8940a1
Selsanat <leandro.benites@edu.devinci.fr>
2025-02-04 10:19:22 +0100

    Tweens, soundmanager, modules structure
    Also fixed ship speed

---
+++ Assets/Scripts/Gamefeel Modules/Modules/TestModule.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestModule : AGameFeelModule
+{
+    public override void ToggleOnGamefeelFeatures()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ToggleOffGamefeelFeatures()
+    {
+        throw new System.NotImplementedException();
+    }
+}
