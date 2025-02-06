9a6fc20a30af524bab5b6b93debb7d138d20e1c3
Selsanat <leandro.benites@edu.devinci.fr>
2025-02-06 11:07:02 +0100

    Defilement Background

---
+++ Assets/Scripts/BackGroundScroll.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroll : MonoBehaviour
+{
+    public float ScrollSpeed;
+    public GameObject[] BackGrounds;
+    private float _BackgroundScroll = 0;
+    
+
+    void Start()
+    {
+        ScrollSpeed = 0.5f;
+    }
+    void Update()
+    {
+        _BackgroundScroll += Time.deltaTime * ScrollSpeed;
+        gameObject.transform.position = new Vector3(0, gameObject.transform.position.y+Time.deltaTime * ScrollSpeed, 0);
+        if (_BackgroundScroll > 10.8f)
+        {
+            _BackgroundScroll -= 10.8f;
+            BackGrounds[0].transform.position = new Vector3(0, BackGrounds[1].transform.position.y - (10.8f*2), 0);
+            GameObject[] temp = new GameObject[BackGrounds.Length];
+            temp[0] = BackGrounds[1];
+            temp[1] = BackGrounds[2];
+            temp[2] = BackGrounds[0];
+            BackGrounds = temp;
+        }
+    }
+}
